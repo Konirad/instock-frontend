790a15e8a60b587f5c0c10a2b1e5b8141f088c58
angelatyk <angelatyk@gmail.com>
2023-08-30 20:43:39 -0400

    Moved Footer to be within BrowerRouter as well to be consistent with Header.

--- src/App.js
+++ src/App.js
@@ -4,30 +4,28 @@ import NotFound from "../src/pages/NotFound/NotFound.js";
 import Warehouses from "../src/pages/Warehouses/Warehouses.js";
 import Inventory from "../src/pages/Inventory/Inventory.js";
 
-
 //Componenets
 
 import Header from "./Components/Header/Header.js";
 import Footer from "./Components/Footer/Footer.js";
-import WarehouseDetails from './Components/WarehouseDetails/WarehouseDetails';
-
+import WarehouseDetails from "./Components/WarehouseDetails/WarehouseDetails";
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
-			<Header/>
+				<Header />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="warehouses" element={<Warehouses />} />
-          <Route path="warehouses/:id" element={<WarehouseDetails />} />
+					<Route path="warehouses/:id" element={<WarehouseDetails />} />
 					<Route path="inventory" element={<Inventory />} />
 					<Route path="*" element={<NotFound />} />
 				</Routes>
+				<Footer />
 			</BrowserRouter>
-			<Footer />
 		</>
-	);  
+	);
 }
 
-export default App;;
+export default App;
