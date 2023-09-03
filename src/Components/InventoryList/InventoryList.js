3fa6602f7cc2c6ce61e582460a58b42fdf7dca63
angelatyk <angelatyk@gmail.com>
2023-09-03 05:23:25 -0400

    Added Table Header row.

--- src/Components/InventoryList/InventoryList.js
+++ src/Components/InventoryList/InventoryList.js
@@ -1,15 +1,41 @@
 import "./InventoryList.scss";
 import InventoryItem from "../InventoryItem/InventoryItem";
+import TableHeader from "../TableHeader/TableHeader";
 
 function InventoryList({ inventoryList }) {
     return (
-        inventoryList && (
-            <div className="inventory-list">
-                {inventoryList.map((inventoryItem) => (
-                    <InventoryItem key={"InventoryItem__" + inventoryItem.id} inventoryItem={inventoryItem} />
-                ))}
+        <>
+            <div className="table-header-row">
+                <div className="column-extra-wide">
+                    <TableHeader label="Inventory Item" sortable="true" />
+                </div>
+                <div className="column-extra-wide">
+                    <TableHeader label="Category" sortable="true" />
+                </div>
+                <div className="column-wide">
+                    <TableHeader label="Status" sortable="true" />
+                </div>
+                <div className="column-normal">
+                    <TableHeader label="QTY" sortable="true" />
+                </div>
+                <div className="column-wide">
+                    <TableHeader label="Warehouse" sortable="true" />
+                </div>
+                <div className="action-label column-normal">
+                    <TableHeader label="Action" sortable="false" />
+                </div>
             </div>
-        )
+            {inventoryList && (
+                <div className="inventory-list">
+                    {inventoryList.map((inventoryItem) => (
+                        <InventoryItem
+                            key={"InventoryItem__" + inventoryItem.id}
+                            inventoryItem={inventoryItem}
+                        />
+                    ))}
+                </div>
+            )}
+        </>
     );
 }
 
