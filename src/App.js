e07e7f84e0be2ac6bd320a7129513419d5168da0
konrad czulowski <konrad.czulowski@gmail.com>
2023-09-04 10:45:08 -0400

    currently trying to figure out how to populate warehouse fields with selected id / warehouse to later give editing abilities

--- src/App.js
+++ src/App.js
@@ -4,34 +4,29 @@ import NotFound from "../src/pages/NotFound/NotFound.js";
 import Warehouses from "../src/pages/Warehouses/Warehouses.js";
 import Inventory from "../src/pages/Inventory/Inventory.js";
 
-
-
 //Componenets
-
 import Header from "./components/Header/Header.js";
 import Footer from "./components/Footer/Footer.js";
 import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
 import AddNewWarehouse from "./components/AddNewWarehouse/AddNewWarehouse.js";
 
 function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<Header />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="warehouses" element={<Warehouses />} />
-					<Route path="warehouses/:id" element={<WarehouseDetails />} />
-                    <Route path="warehouses/:id" element={<WarehouseDetails />} />
-                    <Route path="warehouses/new" element={<AddNewWarehouse />} />
-					<Route path="inventory" element={<Inventory />} />
-					<Route path="*" element={<NotFound />} />
-					
-				</Routes>
-				<Footer />
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="warehouses" element={<Warehouses />} />
+          <Route path="warehouses/:id" element={<WarehouseDetails />} />
+          <Route path="warehouses/new" element={<AddNewWarehouse />} />
+          <Route path="inventory" element={<Inventory />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
